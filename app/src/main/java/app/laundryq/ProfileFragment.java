4fcfd96d12454a84ac1ec2f50c15b573999eed8f
Muhammad Edward Chakim <muhedwardc@gmail.com>
2018-04-24 20:09:17 +0700

    fixing errors

--- app/src/main/java/app/laundryq/ProfileFragment.java
+++ app/src/main/java/app/laundryq/ProfileFragment.java
@@ -1,13 +1,19 @@
 package app.laundryq;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 public class ProfileFragment extends Fragment {
+
+    private Button btnLogout;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,7 +22,20 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        btnLogout = (Button) rootView.findViewById(R.id.logoutBtn);
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), LoginActivity.class);
+                getActivity().startActivity(intent);
+            }
+        });
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        return rootView;
     }
 }
