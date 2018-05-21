66138a0a2ff2b846a46e04c69695870281c9becb
rachmadep <rachmadpribadi@hotmail.com>
2018-05-22 01:54:34 +0700

    Add floating action button Add Laundry

--- app/src/main/java/app/laundryq/HomePegawaiFragment.java
+++ app/src/main/java/app/laundryq/HomePegawaiFragment.java
@@ -1,8 +1,10 @@
 package app.laundryq;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,7 +20,16 @@ public class HomePegawaiFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_home_pegawai, container, false);
+        FloatingActionButton fab = (FloatingActionButton) v.findViewById(R.id.add_laundry);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent in = new Intent(getActivity(), AddLaundryActivity.class);
+                startActivity(in);
+            }
+        });
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_pegawai, container, false);
+        return v;
     }
 }
