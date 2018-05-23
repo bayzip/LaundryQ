d37c54ce90a00ecc41e44803b08573867662417c
Muhammad Edward Chakim <muhedwardc@gmail.com>
2018-05-23 18:47:50 +0700

    Updating UI

--- app/src/main/java/app/laundryq/HomePegawaiFragment.java
+++ app/src/main/java/app/laundryq/HomePegawaiFragment.java
@@ -1,6 +1,8 @@
 package app.laundryq;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -9,6 +11,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 public class HomePegawaiFragment extends Fragment {
@@ -27,6 +30,31 @@ public class HomePegawaiFragment extends Fragment {
             public void onClick(View view) {
                 Intent in = new Intent(getActivity(), AddLaundryActivity.class);
                 startActivity(in);
+            }
+        });
+
+        Button konfirmasiBtn = (Button) v.findViewById(R.id.konfirmasi_btn);
+        konfirmasiBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder alert = new AlertDialog.Builder(getContext());
+                alert.setMessage("Apakah Anda ingin mengkonfirmasi pesanan selesai?");
+
+                alert.setPositiveButton("Ya", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                    }
+                });
+
+                alert.setNegativeButton("Tidak", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+
+                AlertDialog alertDialog = alert.create();
+                alertDialog.show();
             }
         });
         // Inflate the layout for this fragment
